20710bbe65bce880a9b5c1f3275718092752121c
lucciora <lucciora@naver.com>
2016-08-09 23:51:05 +0900

    Signed-off-by: lucciora <lucciora@naver.com>

--- PYTHON_Project/Data_Science/chapter4_matrices.py
+++ PYTHON_Project/Data_Science/chapter4_matrices.py
@@ -32,9 +32,31 @@ def zero_or_one(i, j):
 print(make_matrix(5, 4, zero_or_one))
 
 # matrix 표현, 중심성 중 방향성 있는 네트워크로 표현될 수 있을 듯!
-directed_network = [(0,0), (1,0), (1,1), (2,0), (2,2), (3,0), (3,2), (4,1), (4,4)]
+directed_network = [(0,0), (1,0), (1,1), (2,0), (2,2), (3,0), (3,2), (4,1), (6,4)]
 print(directed_network[1])
-# def make_node_matrix(nodes):
 
+def finding_max_row(nodes_data):
+    maxValue=0
+    for i in nodes_data:
+        if maxValue < i[0]:
+            maxValue=i[0]
+    return maxValue
+
+def finding_max_col(nodes_data):
+    maxValue=0
+    for i in nodes_data:
+        if maxValue < i[1]:
+            maxValue=i[1]
+    return maxValue
+
+ 
+def make_node_matrix(nodes_data, entry_fn):
+    return [[entry_fn(i,j, nodes_data) for j in range(finding_max_col(nodes_data))] 
+            for i in range(finding_max_row(nodes_data))]
+
+def all_zero_but_one(i, j, nodes_data):
+    return 1 if (i, j) in nodes_data else 0
     
+print(make_node_matrix(directed_network, all_zero_but_one))
+# 아직 에러 (6, 4)가 출력되지 않음
     
